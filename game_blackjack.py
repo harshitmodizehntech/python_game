f47b9194374b6fc3c2c969949f7955ee6bc2630b
harshitmodizehntech <harshitmodi.zehntech>
2024-02-15 19:35:25 +0530

    first commit

---
+++ game_blackjack.py
@@ -0,0 +1,101 @@
+import random
+
+class Card(object):
+    def __init__(self, suit, val):
+        self.suit = suit
+        self.value = val
+     
+    def show(self):
+        if self.value == 1:
+            val = "Ace"
+        elif self.value == 11:
+            val = "Jack"
+        elif self.value == 12:
+            val = "Queen"
+        elif self.value == 13:
+            val = "King"
+        else:
+            val = self.value
+
+        return "{} of {}".format(val, self.suit)
+
+
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    # Display all cards in the deck
+    def show(self):
+        for card in self.cards:
+            print (card.show())
+
+    # Generate 52 cards
+    def build(self):
+        self.cards = []
+        for suit in ['Hearts', 'Clubs', 'Diamonds', 'Spades']:
+            for val in range(1,14):
+                self.cards.append(Card(suit, val))
+
+    # Shuffle the deck
+    def shuffle(self, num=1):
+        length = len(self.cards)
+        for _ in range(num):
+            # This is the fisher yates shuffle algorithm
+            for i in range(length-1, 0, -1):
+                random1 = random.randint(0, i)
+                if i == random1:
+                    continue
+                self.cards[i], self.cards[random1] = self.cards[random1], self.cards[i]
+            # You can also use the build in shuffle method
+            # random.shuffle(self.cards)
+
+    # Return the top card
+    def deal(self):
+        return self.cards.pop()
+
+
+class Player(object):
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    def sayHello(self):
+        print ("Hi! My name is {}".format(self.name))
+        return self
+
+    # Draw n number of cards from a deck
+    # Returns true in n cards are drawn, false if less then that
+    def draw(self, deck, num=1):
+        for _ in range(num):
+            card = deck.deal()
+            if card:
+                self.hand.append(card)
+            else: 
+                return False
+        return True
+
+    # Display all the cards in the players hand
+    def showHand(self):
+        print("{}'s hand:".format(self.name))
+        for card in self.hand:
+            print(card.show())
+        return self
+
+
+    def discard(self):
+        return self.hand.pop()
+
+# Test making a Card
+# card = Card('Spades', 6)
+# print card
+
+# Test making a Deck
+myDeck = Deck()
+myDeck.shuffle()
+# deck.show()
+
+bob = Player("Bob")
+bob.sayHello()
+bob.draw(myDeck, 5)
+bob.showHand()
